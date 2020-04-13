f4841cfd7efb6a7a7ea6745c41c6b49da25689de
Jordi <jordis2311@gmail.com>
2020-04-13 20:24:23 -0300

    inits y test

---
+++ tests/test_sumar.py
@@ -0,0 +1,10 @@
+
+from src.bingo import sumar
+from src.bingo import restar
+
+def test_sumar():
+    assert sumar(1,2) == 3
+    assert sumar(1,10) == 11
+
+def test_restar():
+    assert restar(3,1) = 2
