2fce032d53ece4bf5d1a597bc3f1d8f755548c0b
Jordi <jordis2311@gmail.com>
2020-04-14 14:30:35 -0300

    Tarea 1 tests de cantidad casillas y 1 casilla por columna

--- src/bingo.py
+++ src/bingo.py
@@ -1,8 +1,17 @@
 
+# Los 0 son celdas vacias
+# Los 1 son celdas Ocupadas
 def carton():
     carton = (
-        (1,0,0,1,1,0,1,0,1),
-        (0,1,1,1,0,1,1,1,1),
-        (0,1,0,0,1,0,0,1,0)
+        (1,1,0,1,0,1,0,1,1),
+        (1,0,1,0,1,0,1,0,0),
+        (0,1,1,0,0,1,0,1,1)
     )
     return carton
+
+def columna(carton, nro_columna):
+    return(
+        carton[0][nro_columna],
+        carton[1][nro_columna],
+        carton[2][nro_columna]
+    )
--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,10 +1,40 @@
 from src.bingo import carton
+from src.bingo import columna
 
-def test_contar_celdas_ocupadas():
+def test_celdas_15():
     mi_carton = carton()
     contador = 0
     for fila in mi_carton:
         for celda in fila:
             contador = contador + celda
     
-    assert contador == 15 
+    assert contador == 15
+
+def test_celdas_no_mayor_15():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    
+    assert contador <= 15
+
+def test_celdas_no_menor_15():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    
+    assert contador >= 15 
+
+def test_columna_carton():
+    mi_carton = carton()
+    res = True
+    for i in range (0,9):
+        ct = 0
+        (celda1,celda2,celda3) = columna(mi_carton,i)
+        ct = ct + celda1 + celda2 + celda3
+        if (ct == 0):
+            res = False
+    assert res == True
