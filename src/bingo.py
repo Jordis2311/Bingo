ca69c401b6800cbfd525b15f86e9d4618fb1c9b7
Jordi <jordis2311@gmail.com>
2020-05-02 16:35:30 -0300

    Moviendo todas las funciones de test a bingo y comprobando en test si funcionan

--- src/bingo.py
+++ src/bingo.py
@@ -1,6 +1,8 @@
 
 # Los 0 son celdas vacias
 # Los 1 son celdas Ocupadas
+
+#Define un carton
 def carton():
     carton = (
         (1,1,0,1,0,1,0,1,1),
@@ -9,6 +11,7 @@ def carton():
     )
     return carton
 
+#Devuelve una columna del carton
 def columna(carton, nro_columna):
     return(
         carton[0][nro_columna],
@@ -16,6 +19,7 @@ def columna(carton, nro_columna):
         carton[2][nro_columna]
     )
 
+#Revisar si el carton tiene 15 celdas ocupadas
 def validar_quince_celdas(carton):
     contador = 0
     for fila in carton:
@@ -23,3 +27,43 @@ def validar_quince_celdas(carton):
             if celda != 0:
                 contador = contador + 1
     return contador == 15
+
+#Revisar si el carton tiene maximo 15 celdas ocupadas
+def validar_no_mayor_quince(carton):
+    contador = 0
+    for fila in carton:
+        for celda in fila:
+            if celda != 0:
+                contador = contador + 1
+    return contador <= 15
+
+#Revisar si el carton tiene minimo 15 celdas ocupadas
+def validar_no_menor_quince(carton):
+    contador = 0
+    for fila in carton:
+      for celda in fila:
+          if celda != 0:
+            contador = contador + 1
+    return contador >= 15
+
+#Revisa si cada columna tiene al menos 1 celda ocupada
+def todas_columnas_con_numeros(carton):
+    res = True
+    for i in range (0,9):
+        ct = 0
+        (celda1,celda2,celda3) = columna(carton,i)
+        ct = ct + celda1 + celda2 + celda3
+        if (ct == 0):
+            res = False
+    return res == True
+
+#Revias si cada fila tiene al menos 1 celda ocupada
+def todas_filas_con_numeros(carton):
+    res = True
+    for fila in carton:
+        ct = 0
+        for celda in fila:
+            ct = ct + celda
+        if (ct == 0):
+            res = False
+    return res == True
--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,53 +1,52 @@
 from src.bingo import carton
 from src.bingo import columna
 from src.bingo import validar_quince_celdas
+from src.bingo import validar_no_mayor_quince
+from src.bingo import validar_no_menor_quince
+from src.bingo import todas_columnas_con_numeros
+from src.bingo import todas_filas_con_numeros
 
-
-#Revisar si el carton tiene 15 celdas ocupadas
+#Revisar si la funcion que valida que el carton tiene 15 celdas ocupadas funciona correctamente
 def test_celdas_15():
-    mi_carton = carton()
+    mi_carton = (
+     (1,0,0,1,1,0,1,1,0),
+     (0,1,0,0,0,1,1,0,1),
+     (0,0,1,1,1,0,1,1,0)
+    )
     assert validar_quince_celdas(mi_carton) == True
 
-#Revisar si el carton tiene maximo 15 celdas ocupadas
+#Revisar si la funcion que valida que el carton tiene maximo 15 celdas ocupadas funciona correctamente
 def test_celdas_no_mayor_15():
-    mi_carton = carton()
-    contador = 0
-    for fila in mi_carton:
-        for celda in fila:
-            contador = contador + celda
-    
-    assert contador <= 15
+    mi_carton = (
+     (1,0,0,1,1,0,1,1,0),
+     (0,1,0,0,0,1,1,0,1),
+     (0,1,1,1,1,0,1,1,0)
+    )
+    assert validar_no_mayor_quince(mi_carton) == True
 
-#Revisar si el carton tiene minimo 15 celdas ocupadas
+#Revisar si la funcion que valida que el carton tiene minimo 15 celdas ocupadas funciona correctamente
 def test_celdas_no_menor_15():
-    mi_carton = carton()
-    contador = 0
-    for fila in mi_carton:
-        for celda in fila:
-            contador = contador + celda
-    
-    assert contador >= 15 
+    mi_carton = (
+     (1,0,0,1,1,1,0,1,0),
+     (1,0,0,0,0,1,1,0,1),
+     (0,1,1,1,1,0,1,0,1)
+    )
+    assert validar_no_menor_quince(mi_carton) == True
 
-#Revisa si cada columna tiene al menos 1 celda ocupada
+#Revisa si la funcion que valida que cada columna tiene al menos 1 celda ocupada funciona correctamente
 def test_columna_carton():
-    mi_carton = carton()
-    res = True
-    for i in range (0,9):
-        ct = 0
-        (celda1,celda2,celda3) = columna(mi_carton,i)
-        ct = ct + celda1 + celda2 + celda3
-        if (ct == 0):
-            res = False
-    assert res == True
+    mi_carton = (
+     (1,0,1,1,0,1,0,1,0),
+     (1,0,0,0,0,1,1,0,1),
+     (0,1,1,1,1,0,0,1,1)
+    )
+    assert todas_columnas_con_numeros(mi_carton) == True
 
-#Revias si cada fila tiene al menos 1 celda ocupada
+#Revias si la funcion que valida que cada fila tiene al menos 1 celda ocupada funciona correctamente
 def test_fila_carton():
-    mi_carton = carton()
-    res = True
-    for fila in mi_carton:
-        ct = 0
-        for celda in fila:
-            ct = ct + celda
-        if (ct == 0):
-            res = False
-    assert res == True
+    mi_carton = (
+     (1,1,0,1,0,1,0,1,0),
+     (1,0,1,0,1,0,1,0,1),
+     (0,1,0,1,1,0,0,1,1)
+    )
+    assert todas_filas_con_numeros(mi_carton) == True
